5ef59f9f95ab6f8ef9ec419309bc681ded3825d9
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-09-29 10:01:47 +0330

    add border to nav-links

--- js/main.js
+++ js/main.js
@@ -6,3 +6,19 @@ window.addEventListener('load', () => {
         once: true,
     });
 })
+document.addEventListener('DOMContentLoaded', function() {
+    const message = document.querySelector('#omid-message').innerHTML
+
+
+    document.querySelector('#omid-message').textContent = ""
+    document.querySelector('#omid-message').innerHTML = ''
+
+
+    var _ = new Typed('#omid-message', {
+        strings: [message],
+        startDelay: 1000,
+        typeSpeed: 50,
+        loop: false
+    })
+
+})
