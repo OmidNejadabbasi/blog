ace080e21aae4f2ec530fa662dbbb2b4b2c15207
Omid Nejadabbasi <nejadabbasio@gmail.com>
2021-10-01 09:26:22 +0330

    1 October

--- js/main.js
+++ js/main.js
@@ -1,24 +1,40 @@
 /*   AOS initialization   */
 
-window.addEventListener('load', () => {
-    AOS.init({
-        duration: 1000,
-        once: true,
-    });
+
+AOS.init({
+    duration: 1000,
+    once: true,
+});
+
+const message = document.querySelector('#omid-message').innerHTML
+
+
+document.querySelector('#omid-message').textContent = ""
+document.querySelector('#omid-message').innerHTML = ''
+
+
+var _ = new Typed('#omid-message', {
+    strings: [message],
+    startDelay: 1000,
+    typeSpeed: 50,
+    loop: false
 })
-document.addEventListener('DOMContentLoaded', function() {
-    const message = document.querySelector('#omid-message').innerHTML
+
+// Header fixed to top on scroll
+const header = document.querySelector('#header')
+let headerOffset = header.offsetTop
+let nextElement = header.nextElementSibling
+
+function fixHeader() {
 
 
-    document.querySelector('#omid-message').textContent = ""
-    document.querySelector('#omid-message').innerHTML = ''
-
-
-    var _ = new Typed('#omid-message', {
-        strings: [message],
-        startDelay: 1000,
-        typeSpeed: 50,
-        loop: false
-    })
-
-})
+    if ((headerOffset - window.scrollY) <= 0) {
+        header.classList.add('fixed-top')
+        nextElement.classList.add('scrolled-offset')
+    } else {
+        header.classList.remove('fixed-top')
+        nextElement.classList.remove('scrolled-offset')
+    }
+}
+document.addEventListener('scroll', fixHeader)
+window.onload = fixHeader
